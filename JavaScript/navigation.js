fab3e48852bc10945f38f0ecff78fcb4a30ef7b5
sewert <spencer.ewert@gmail.com>
2018-08-17 13:56:26 -0600

    Files from old website

---
+++ JavaScript/navigation.js
@@ -0,0 +1,34 @@
+function createNavigationLinks()
+{
+	var links = document.getElementById("heading");
+	
+	var contents = "" +
+	"<table>" +
+	"<tr>" +
+	"<td><a href=\"AlTroth.html\">AL TROTH</a></td>" +
+	"<td><a href=\"CalBird.html\">CAL BIRD</a></td>" +
+	"<td><a href=\"CarrieStevens.html\">CARRIE STEVENS</a></td>" +
+	"<td><a href=\"CharlesBrooks.html\">CHARLES BROOKS</a></td>" +
+	"<td><a href=\"CraigMatthews.html\">CRAIG MATTHEWS</a></td>" +
+	"<td><a href=\"DonMartinez.html\">DON MARTINEZ</a></td>" +
+	"</tr>" +
+	"<tr>" +
+	"<td><a href=\"DrErnestSchwiebert.html\">DR. ERNEST SCHWIEBERT</a></td>" +
+	"<td><a href=\"FranzPott.html\">FRANZ POTT</a></td>" +
+	"<td><a href=\"GeorgeCroonenberghs.html\">GEORGE CROONENBERGHS</a></td>" +
+	"<td><a href=\"GeorgeGrant.html\">GEORGE GRANT</a></td>" +
+	"<td><a href=\"HelenShaw.html\">HELEN SHAW</a></td>" +
+	"<td><a href=\"JackBoehme.html\">JACK BOEHME</a></td>" +
+	"</tr>" +
+	"<tr>" +
+	"<td><a href=\"NormanMeans.html\">NORMAN MEANS</a></td>" +
+	"<td><a href=\"OtherAmericanTyers.html\">OTHER AMERICAN TYERS</a></td>" +
+	"<td><a href=\"OtherYellowstoneTyers.html\">OTHER YELLOWSTONE TYERS</a></td>" +
+	"<td><a href=\"PatSigBarnes.html\">PAT & SIG BARNES</a></td>" +
+	"<td><a href=\"PollyRosborough.html\">POLLY ROSBOROUGH</a></td>" +
+	"<td><a href=\"index.html\">WELCOME</a></td>" +
+	"</tr>" +
+	"</table>";
+	
+	links.innerHTML = contents;
+}
